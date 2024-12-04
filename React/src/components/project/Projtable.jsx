ae5ce59aeba25b624cecba2ccd3b7b5be74c2db0
Lee4405 <leehl1996@naver.com>
2024-12-04 17:40:37 +0900

    대시보드_프로젝트_테이블4
    1. 진행률 추가

--- React/src/components/project/Projtable.jsx
+++ React/src/components/project/Projtable.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-import { useEffect,useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { useEffect,useState } from "react";
+import { ProgressBar } from "react-bootstrap";
 import {
     Input,
     Button,
@@ -13,7 +14,9 @@ import {
     CardTitle,
     Table,
     Row,
+    Progress,
   } from "reactstrap";
+
 
 
 export default function Projtable(){
@@ -21,7 +24,7 @@ export default function Projtable(){
     //나중에 reducer 공유자원에서 가져올 compNum
     //const compNum = useSelector((state) => state.user.user_fk_comp_num);
     const compNum = 1; //테스트용 임시 값 지워주기
-
+    const testNum = 50;
     const showList = () => {
         axios.get(`/board/${compNum}`)
         .then((res)=>{
@@ -40,6 +43,7 @@ export default function Projtable(){
     
     useEffect(()=>{
         showList();
+        
     },[]);
 
     const navigate = useNavigate();
@@ -55,22 +59,25 @@ export default function Projtable(){
         const month = (date.getMonth() + 1).toString().padStart(2,'0');
         const day = date.getDate().toString().padStart(2,'0');
 
-        let yyyy_mm_dd = `${year}.${month}.${day}`;
+        let yyyy_mm_dd = `${year}-${month}-${day}`;
         return yyyy_mm_dd;
       };
+
 
     return(
         <Card className="shadow rounded" style={{ marginTop: "20px", marginLeft: "15px", marginRight: "15px" }}>
         <CardBody>
         <div className="d-flex justify-content-between align-items-center mb-4">
-          <h2 className="text-center mb-4">프로젝트 목록</h2>
-            <Button onClick={()=>{gotoProjLists(compNum)}}>더보기</Button>
+        <h3 className="text-center fw-bold w-100 mb-4">프로젝트 테이블</h3>
+            <Button outline color="primary" style={{ whiteSpace: "nowrap", padding: "5px 15px" }} onClick={() => gotoProjLists(compNum)}>
+                더 보기
+            </Button>
         </div>
           <Row>
             <Col sm={12}>                      
               <Table striped>
                 <thead>
-                  <tr>
+                  <tr style={{ fontSize: "12px", color: "gray",textAlign:"center" }}>
                     <th>프로젝트</th>
                     <th>담당자</th>
                     <th>상태</th>
@@ -85,15 +92,26 @@ export default function Projtable(){
                     </tr>
                   ) : (
                     projs.map((proj) => (
-                      <tr key={proj.proj_pk_num}>
-                        <td>{proj.proj_name}</td>
-                        <td>{proj.proj_username}<br/>
-                            {proj.proj_userMail}
+                      <tr key={proj.proj_pk_num} style={{ fontSize: "13px",textAlign:"center" }}>
+                        <td style={{ fontWeight: "bold", fontSize: "15px"}}>{proj.proj_name}</td>
+                        <td style={{ fontWeight: "bold", fontSize: "11px"}}>{proj.proj_username}<br/>
+                        <div style={{ fontSize: "10px", color: "gray",textAlign:"center" }}>
+                            ({proj.proj_userMail})
+                        </div>
                         </td>
                         <td>{proj.proj_status}</td>
                         <td>{dateForm(proj.proj_enddate)}</td>
-                        <td>{proj.proj_progress}</td>
-                        <td><div class="dropdown"></div></td>
+                        <td>
+                          <Progress value={proj.proj_progress} max={100}></Progress>
+                          <div  style={{
+                                fontSize: "10px", // 글씨 크기
+                                color: "#A0A0A 0", // 회색 텍스트
+                                marginTop: "5px", // 진행 바와 텍스트 간격
+                                textAlign:"center"
+                                }}>
+                                  {`진행률 : ${proj.proj_progress ||0}%`}
+                           </div>
+                        </td>
                       </tr>
                     ))
                   )}
