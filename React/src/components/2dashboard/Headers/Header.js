3cec1b1bed78bb743b2fdfc69612c9c1d5fd6289
문서현 <dongyin1123@hanmail.net>
2024-12-05 18:58:58 +0900

    헤더가 너무 길어서 줄여놓은 상태

--- React/src/components/2dashboard/Headers/Header.js
+++ React/src/components/2dashboard/Headers/Header.js
@@ -25,9 +25,10 @@ import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
 const Header = () => {
   return (
     <>
-      <div className="header bg-gradient-info pb-6 pt-5 pt-md-8"></div>
-    </>
+      <div className="header bg-gradient-info pt-md-8"></div> 
+    </> 
   );
 };
 
+//  pb-6 이거 넣으면 헤더 길어짐
 export default Header;
